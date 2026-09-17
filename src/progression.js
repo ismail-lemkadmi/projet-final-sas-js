141daf2497e8e3566ae128f587f2a8bee9841dd4
ismail lemkadmi <ismaillemkadmi001@gmail.com>
2026-09-17 17:17:14 +0100

    Ajouter l'enregistrement des resultats

--- src/progression.js
+++ src/progression.js
@@ -1,3 +1,6 @@
+const apprenants = require("./data");
+
+
 function normaliserNom(nom){
     nom = nom.trim()
     nom = nom.toLowerCase();
@@ -17,4 +20,53 @@ if (exercicesTermines < 0 || totalExercices < 0) {
     return false;
 }
 return true;
+}
+function ajouterApprenant(id, nomComplet, ville) {
+for (let i = 0 ; i < apprenants.length; i++) {
+    if (apprenants[i].id === id) {
+    console.log("Erreur : cet identifiant existe déjà.");
+    return false;
+}
+}
+const nouvelApprenant = {
+    id: id,
+    nomComplet: normaliserNom(nomComplet),
+    ville: ville,
+    resultats: []
+};
+apprenants.push(nouvelApprenant);
+return true;
+}
+function rechercherApprenant(id) {
+for (let i = 0; i < apprenants.length; i++) {
+    if (apprenants[i].id === id) {
+    return apprenants[i];
+}
+}
+return null;
+}
+function enregistrerResultat(id, jour, exercicesTermines, totalExercices, challengeTermine) {
+const apprenant = rechercherApprenant(id);
+if (apprenant === null) {
+    console.log("Erreur : apprenant introuvable.");
+    return false;
+}
+if (!validerResultat(jour, exercicesTermines, totalExercices)) {
+    return false;
+}
+const nouveauResultat = {
+    jour: jour,
+    exercicesTermines: exercicesTermines,
+    totalExercices: totalExercices,
+    challengeTermine: challengeTermine
+};
+for (let i = 0; i < apprenant.resultats.length; i++) {
+if (apprenant.resultats[i].jour === jour) {
+    apprenant.resultats[i] = nouveauResultat;
+    return true;
+}
+}
+apprenant.resultats.push(nouveauResultat);
+return true;
+
 }
